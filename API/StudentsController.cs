8e0c965684e4ca70981a00da1f8c59e030bb101d
tegeskog <tomas.tegeskog@gmail.com>
2015-12-01 13:35:28 +0100

    ny
    new upplaga

---
+++ API/StudentsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WU15.StudentAdministration.Web.Models;
+
+namespace WU15.StudentAdministration.Web.API
+{
+    public class StudentsController : ApiController
+    {
+        public IEnumerable<Student> Get()
+        {
+            return MvcApplication.Students;
+        }
+
+        public Student Get(int id)
+        {
+            return MvcApplication.Students.FirstOrDefault(x => x.Id == id);
+        }
+        [HttpPost]
+        public string Post(Student student)
+        {
+            if (student.Id == 0)
+            {
+                if (MvcApplication.Students.Any())
+                {
+                    var id = MvcApplication.Students.Max(x => x.Id) + 1;
+                    student.Id = id;
+                }
+                else
+                {
+                    student.Id = 1;
+                }
+            }
+            else
+            {
+                var savedIndex = MvcApplication.Students.FindIndex(x => x.Id == student.Id);
+                MvcApplication.Students.RemoveAt(savedIndex);
+            }
+            MvcApplication.Students.Add(student);
+
+            return student.Name;
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            var student = MvcApplication.Students.FirstOrDefault(x => x.Id == id);
+            MvcApplication.Students.Remove(student);
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace WU15.StudentAdministration.Web.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Personnummer {get; set;}
+        public bool Active {get; set;}
+    }
+}
